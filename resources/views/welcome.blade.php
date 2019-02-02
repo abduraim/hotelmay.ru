80b1bb04e5640a4c0dbc169b344ed524691bc6ea
abduraim.t <abduraim.t@gmail.com>
2019-02-02 15:38:25 +0300

    style tests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,26 +7,13 @@
 
         <title>Hotelmay</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-
-            </div>
-        </div>
+        <section id="main" class="section">
+            Hotelmay
+        </section>
     </body>
 </html>
