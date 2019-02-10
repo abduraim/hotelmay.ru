fdc62bf926d2551ca1ded85f6a3d62d3b2735d45
abduraim.t <abduraim.t@gmail.com>
2019-02-11 02:18:34 +0300

    start section room

--- resources/views/sections/rooms.blade.php
+++ resources/views/sections/rooms.blade.php
@@ -1,6 +1,45 @@
 <section id="rooms" class="section">
-    Rooms
-    @foreach($rooms as $room)
-        <h1>{{$room->title}}</h1>
-    @endforeach
+    <div class="wrapper">
+        <h1>Наши номера</h1>
+        <ul class="rooms_list">
+            <li class="rooms_item">
+                <div class="rooms_title"><h3>Апартаменты "Delux" с&nbsp;кухней</h3></div>
+                <div class="rooms_photo">Photo</div>
+                <div class="rooms_cost">1212 rub.</div>
+                <div class="rooms_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque eligendi id libero necessitatibus nisi numquam, quaerat reiciendis? Alias cupiditate ea eum inventore libero perspiciatis possimus quibusdam quo repudiandae veritatis?</div>
+                <a href="#" class="btn_show_modal">Забронировать</a>
+            </li>
+            <li class="rooms_item">
+                <div class="rooms_title">Title</div>
+                <div class="rooms_photo">Photo</div>
+                <div class="rooms_cost">1212 rub.</div>
+                <div class="rooms_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque eligendi id libero necessitatibus nisi numquam, quaerat reiciendis? Alias cupiditate ea eum inventore libero perspiciatis possimus quibusdam quo repudiandae veritatis?</div>
+                <a href="#" class="btn_show_modal">Забронировать</a>
+            </li>
+            <li class="rooms_item">
+                <div class="rooms_title">Title</div>
+                <div class="rooms_photo">Photo</div>
+                <div class="rooms_cost">1212 rub.</div>
+                <div class="rooms_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque eligendi id libero necessitatibus nisi numquam, quaerat reiciendis? Alias cupiditate ea eum inventore libero perspiciatis possimus quibusdam quo repudiandae veritatis?</div>
+                <a href="#" class="btn_show_modal">Забронировать</a>
+            </li>
+            <li class="rooms_item">
+                <div class="rooms_title">Title</div>
+                <div class="rooms_photo">Photo</div>
+                <div class="rooms_cost">1212 rub.</div>
+                <div class="rooms_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque eligendi id libero necessitatibus nisi numquam, quaerat reiciendis? Alias cupiditate ea eum inventore libero perspiciatis possimus quibusdam quo repudiandae veritatis?</div>
+                <a href="#" class="btn_show_modal">Забронировать</a>
+            </li>
+            <li class="rooms_item">
+                <div class="rooms_title">Title</div>
+                <div class="rooms_photo">Photo</div>
+                <div class="rooms_cost">1212 rub.</div>
+                <div class="rooms_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque eligendi id libero necessitatibus nisi numquam, quaerat reiciendis? Alias cupiditate ea eum inventore libero perspiciatis possimus quibusdam quo repudiandae veritatis?</div>
+                <a href="#" class="btn_show_modal">Забронировать</a>
+            </li>
+        </ul>
+        {{--@foreach($rooms as $room)--}}
+            {{--<h1>{{$room->title}}</h1>--}}
+        {{--@endforeach--}}
+    </div>
 </section>
