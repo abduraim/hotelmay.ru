9cca56539b7abca07a51b7c06bc9aa42b19b2d25
abduraim.t <abduraim.t@gmail.com>
2019-02-10 12:26:14 +0300

    add benefits section

--- resources/views/sections/benefits.blade.php
+++ resources/views/sections/benefits.blade.php
@@ -1,21 +1,55 @@
 <section id="benefits" class="section">
-    Benefits
-    <div class="icon">
-        @include('svg.kitchen')
-    </div>
-    <div class="icon">
-        @include('svg.cleaning')
-    </div>
-    <div class="icon">
-        @include('svg.eurorepair')
-    </div>
-    <div class="icon">
-        @include('svg.wifi')
-    </div>
-    <div class="icon">
-        @include('svg.bathroom')
-    </div>
-    <div class="icon">
-        @include('svg.tv')
+    <div class="wrapper">
+        <h1>Наши преимущества</h1>
+        <ul class="benefits_block">
+            <li class="benefits_item">
+                <div class="benefits_item__icon">
+                    @include('svg.kitchen')
+                </div>
+                <div class="benefits_item__description">
+                    <h3>Апартаменты с персональной кухней</h3>
+                </div>
+            </li>
+            <li class="benefits_item">
+                <div class="benefits_item__icon">
+                    @include('svg.cleaning')
+                </div>
+                <div class="benefits_item__description">
+                    <h3>Уборка номеров</h3>
+                </div>
+            </li>
+            <li class="benefits_item">
+                <div class="benefits_item__icon">
+                    @include('svg.eurorepair')
+                </div>
+                <div class="benefits_item__description">
+                    <h3>Евроремонт</h3>
+                </div>
+            </li>
+            <li class="benefits_item">
+                <div class="benefits_item__icon">
+                    @include('svg.wifi')
+                </div>
+                <div class="benefits_item__description">
+                    <h3>Wi-fi</h3>
+                </div>
+            </li>
+            <li class="benefits_item">
+                <div class="benefits_item__icon">
+                    @include('svg.bathroom')
+                </div>
+                <div class="benefits_item__description">
+                    <h3>Персональный санузел в каждом номере</h3>
+                </div>
+            </li>
+            <li class="benefits_item">
+                <div class="benefits_item__icon">
+                    @include('svg.tv')
+                </div>
+                <div class="benefits_item__description">
+                    <h3>TV в каждом номере</h3>
+                </div>
+            </li>
+        </ul>
     </div>
 </section>
